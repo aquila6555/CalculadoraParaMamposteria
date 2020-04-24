43af9a73827662e5bcd5111b1bced32d4181a510
JDeveloper <aquila6555@gmail.com>
2020-04-24 13:22:03 -0400

    primer control git

---
+++ CalculadoraParaMamposteria/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace CalculadoraParaMamposteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroLabelHeader1 = new MetroSuite.MetroLabelHeader();
+            this.txtLadrilloAncho = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtLadrilloAlto = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtLadrilloLargo = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtMurLargo = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtMuroAlto = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.cboTipoMortero = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.metroLabelHeader2 = new MetroSuite.MetroLabelHeader();
+            this.metroLabelHeader3 = new MetroSuite.MetroLabelHeader();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabelHeader1
+            // 
+            this.metroLabelHeader1.AutoSize = true;
+            this.metroLabelHeader1.Font = new System.Drawing.Font("Segoe UI Light", 16F, System.Drawing.FontStyle.Bold);
+            this.metroLabelHeader1.ForeColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(240)))));
+            this.metroLabelHeader1.ForeColorNormal = System.Drawing.Color.Black;
+            this.metroLabelHeader1.Location = new System.Drawing.Point(132, 38);
+            this.metroLabelHeader1.Name = "metroLabelHeader1";
+            this.metroLabelHeader1.Size = new System.Drawing.Size(312, 30);
+            this.metroLabelHeader1.TabIndex = 3;
+            this.metroLabelHeader1.Text = "Calculadora de mamposteria ";
+            this.metroLabelHeader1.UseHoverMode = false;
+            // 
+            // txtLadrilloAncho
+            // 
+            this.txtLadrilloAncho.Depth = 0;
+            this.txtLadrilloAncho.Hint = "Ancho";
+            this.txtLadrilloAncho.Location = new System.Drawing.Point(145, 36);
+            this.txtLadrilloAncho.MaxLength = 32767;
+            this.txtLadrilloAncho.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtLadrilloAncho.Name = "txtLadrilloAncho";
+            this.txtLadrilloAncho.PasswordChar = '\0';
+            this.txtLadrilloAncho.SelectedText = "";
+            this.txtLadrilloAncho.SelectionLength = 0;
+            this.txtLadrilloAncho.SelectionStart = 0;
+            this.txtLadrilloAncho.Size = new System.Drawing.Size(61, 23);
+            this.txtLadrilloAncho.TabIndex = 6;
+            this.txtLadrilloAncho.TabStop = false;
+            this.txtLadrilloAncho.UseSystemPasswordChar = false;
+            // 
+            // txtLadrilloAlto
+            // 
+            this.txtLadrilloAlto.Depth = 0;
+            this.txtLadrilloAlto.Hint = "Alto";
+            this.txtLadrilloAlto.Location = new System.Drawing.Point(84, 36);
+            this.txtLadrilloAlto.MaxLength = 32767;
+            this.txtLadrilloAlto.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtLadrilloAlto.Name = "txtLadrilloAlto";
+            this.txtLadrilloAlto.PasswordChar = '\0';
+            this.txtLadrilloAlto.SelectedText = "";
+            this.txtLadrilloAlto.SelectionLength = 0;
+            this.txtLadrilloAlto.SelectionStart = 0;
+            this.txtLadrilloAlto.Size = new System.Drawing.Size(61, 23);
+            this.txtLadrilloAlto.TabIndex = 7;
+            this.txtLadrilloAlto.TabStop = false;
+            this.txtLadrilloAlto.UseSystemPasswordChar = false;
+            // 
+            // txtLadrilloLargo
+            // 
+            this.txtLadrilloLargo.Depth = 0;
+            this.txtLadrilloLargo.Hint = "Largo ";
+            this.txtLadrilloLargo.Location = new System.Drawing.Point(6, 36);
+            this.txtLadrilloLargo.MaxLength = 32767;
+            this.txtLadrilloLargo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtLadrilloLargo.Name = "txtLadrilloLargo";
+            this.txtLadrilloLargo.PasswordChar = '\0';
+            this.txtLadrilloLargo.SelectedText = "";
+            this.txtLadrilloLargo.SelectionLength = 0;
+            this.txtLadrilloLargo.SelectionStart = 0;
+            this.txtLadrilloLargo.Size = new System.Drawing.Size(61, 23);
+            this.txtLadrilloLargo.TabIndex = 8;
+            this.txtLadrilloLargo.TabStop = false;
+            this.txtLadrilloLargo.UseSystemPasswordChar = false;
+            // 
+            // txtMurLargo
+            // 
+            this.txtMurLargo.Depth = 0;
+            this.txtMurLargo.Hint = "Largo ";
+            this.txtMurLargo.Location = new System.Drawing.Point(25, 36);
+            this.txtMurLargo.MaxLength = 32767;
+            this.txtMurLargo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtMurLargo.Name = "txtMurLargo";
+            this.txtMurLargo.PasswordChar = '\0';
+            this.txtMurLargo.SelectedText = "";
+            this.txtMurLargo.SelectionLength = 0;
+            this.txtMurLargo.SelectionStart = 0;
+            this.txtMurLargo.Size = new System.Drawing.Size(61, 23);
+            this.txtMurLargo.TabIndex = 12;
+            this.txtMurLargo.TabStop = false;
+            this.txtMurLargo.UseSystemPasswordChar = false;
+            // 
+            // txtMuroAlto
+            // 
+            this.txtMuroAlto.Depth = 0;
+            this.txtMuroAlto.Hint = "Alto";
+            this.txtMuroAlto.Location = new System.Drawing.Point(92, 36);
+            this.txtMuroAlto.MaxLength = 32767;
+            this.txtMuroAlto.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtMuroAlto.Name = "txtMuroAlto";
+            this.txtMuroAlto.PasswordChar = '\0';
+            this.txtMuroAlto.SelectedText = "";
+            this.txtMuroAlto.SelectionLength = 0;
+            this.txtMuroAlto.SelectionStart = 0;
+            this.txtMuroAlto.Size = new System.Drawing.Size(61, 23);
+            this.txtMuroAlto.TabIndex = 11;
+            this.txtMuroAlto.TabStop = false;
+            this.txtMuroAlto.UseSystemPasswordChar = false;
+            // 
+            // cboTipoMortero
+            // 
+            this.cboTipoMortero.FormattingEnabled = true;
+            this.cboTipoMortero.Items.AddRange(new object[] {
+            "1:2",
+            "1:3",
+            "1:4",
+            "1:5",
+            "1:6"});
+            this.cboTipoMortero.Location = new System.Drawing.Point(27, 36);
+            this.cboTipoMortero.Name = "cboTipoMortero";
+            this.cboTipoMortero.Size = new System.Drawing.Size(48, 21);
+            this.cboTipoMortero.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLadrilloLargo);
+            this.groupBox1.Controls.Add(this.txtLadrilloAlto);
+            this.groupBox1.Controls.Add(this.txtLadrilloAncho);
+            this.groupBox1.Location = new System.Drawing.Point(23, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 88);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dimensiones del ladrillo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMurLargo);
+            this.groupBox2.Controls.Add(this.txtMuroAlto);
+            this.groupBox2.Location = new System.Drawing.Point(358, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(212, 88);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Propiedades del muro";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cboTipoMortero);
+            this.groupBox3.Location = new System.Drawing.Point(241, 91);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(111, 88);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "tipo de mortero";
+            // 
+            // metroLabelHeader2
+            // 
+            this.metroLabelHeader2.AutoSize = true;
+            this.metroLabelHeader2.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.metroLabelHeader2.ForeColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(240)))));
+            this.metroLabelHeader2.ForeColorNormal = System.Drawing.Color.Black;
+            this.metroLabelHeader2.Location = new System.Drawing.Point(227, 17);
+            this.metroLabelHeader2.Name = "metroLabelHeader2";
+            this.metroLabelHeader2.Size = new System.Drawing.Size(104, 21);
+            this.metroLabelHeader2.TabIndex = 21;
+            this.metroLabelHeader2.Text = "Programacion";
+            this.metroLabelHeader2.UseHoverMode = false;
+            // 
+            // metroLabelHeader3
+            // 
+            this.metroLabelHeader3.AutoSize = true;
+            this.metroLabelHeader3.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.metroLabelHeader3.ForeColorHover = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(164)))), ((int)(((byte)(240)))));
+            this.metroLabelHeader3.ForeColorNormal = System.Drawing.Color.Black;
+            this.metroLabelHeader3.Location = new System.Drawing.Point(353, 241);
+            this.metroLabelHeader3.Name = "metroLabelHeader3";
+            this.metroLabelHeader3.Size = new System.Drawing.Size(223, 30);
+            this.metroLabelHeader3.TabIndex = 22;
+            this.metroLabelHeader3.Text = "Mireya Quisbert Torrez";
+            this.metroLabelHeader3.UseHoverMode = false;
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.Pink;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.White;
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.Pink;
+            this.bunifuThinButton21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "Calcular";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.Purple;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.Purple;
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.Purple;
+            this.bunifuThinButton21.Location = new System.Drawing.Point(231, 197);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(127, 41);
+            this.bunifuThinButton21.TabIndex = 18;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.bunifuThinButton21_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CalculadoraParaMamposteria.Properties.Resources.a;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 195);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CalculadoraParaMamposteria.Properties.Resources.s;
+            this.pictureBox2.Location = new System.Drawing.Point(510, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 73);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 399);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.metroLabelHeader3);
+            this.Controls.Add(this.metroLabelHeader2);
+            this.Controls.Add(this.bunifuThinButton21);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroLabelHeader1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.96D;
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Pink;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroSuite.MetroLabelHeader metroLabelHeader1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtLadrilloAncho;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtLadrilloAlto;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtLadrilloLargo;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtMurLargo;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtMuroAlto;
+        private System.Windows.Forms.ComboBox cboTipoMortero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroSuite.MetroLabelHeader metroLabelHeader2;
+        private MetroSuite.MetroLabelHeader metroLabelHeader3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ CalculadoraParaMamposteria/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+
+namespace CalculadoraParaMamposteria
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+           
+                        
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            //calculamos el area del muro
+            double altoM, areaM, largoM;
+
+            altoM = double.Parse(txtMuroAlto.Text);
+
+            largoM = double.Parse(txtMurLargo.Text);
+
+            areaM = altoM * largoM;
+
+            //valores de la junta
+            double junta;
+            junta = 1.5;
+
+            //calculamos el area del ladrillo + junta
+            double altoL, largoL, anchoL, areaL, altoLJ, largoLJ;
+
+            anchoL = double.Parse(txtLadrilloAncho.Text);
+            anchoL = anchoL / 100;
+
+            altoL = double.Parse(txtLadrilloAlto.Text);
+
+            altoLJ = (altoL + junta) / 100;
+
+            altoL = altoL / 100;
+
+
+
+
+            largoL = double.Parse(txtLadrilloLargo.Text);
+            largoLJ = (largoL + junta) / 100;
+
+            largoL = largoL / 100;
+
+            areaL = largoLJ * altoLJ;
+
+            //cantidad de ladrillos
+            double CantLadri;
+
+            CantLadri = areaM / areaL;
+
+            if (CantLadri < 0.5)
+            {
+                CantLadri = Math.Round(CantLadri);
+                MessageBox.Show("la cantidad de ladrillos necesaria es: " + Math.Round(CantLadri).ToString());
+            }
+            else
+            {
+                CantLadri = Math.Ceiling(CantLadri);
+                MessageBox.Show("la cantidad de ladrillos necesaria es: " + Math.Ceiling(CantLadri).ToString());
+            }
+
+
+            //calcular el volumen del mortero
+
+            double vMortero;
+            vMortero = (largoM * altoM * anchoL) - (CantLadri * largoL * altoL * anchoL);
+
+           
+
+
+
+            double arena = 0, cemento = 0, agua = 0;
+
+
+            switch (cboTipoMortero.Text)
+            {
+                case "1:2": cemento = 610; arena = 0.97; agua = 250; break;
+                case "1:3": cemento = 454; arena = 1.10; agua = 250; break;
+                case "1:4": cemento = 364; arena = 1.16; agua = 240; break;
+                case "1:5": cemento = 302; arena = 1.20; agua = 240; break;
+                case "1:6": cemento = 261; arena = 1.20; agua = 235; break;
+            }
+
+            //calculamos la cantidad de cemento 
+
+            cemento = (cemento * vMortero) * 1.05;
+
+            //calculamos la cantidad de arena
+
+            arena = (arena * vMortero);
+
+            //calculamos la cantidad de agua
+
+            agua = (agua * vMortero);
+
+            cemento = Math.Ceiling(cemento);
+            MessageBox.Show("la cantidad de cemento necesaria es: " + cemento.ToString());
+
+            MessageBox.Show("la cantidad de arena necesaria es: " + arena.ToString());
+            agua = Math.Ceiling(agua);
+            MessageBox.Show("la cantidad de agua necesaria es: " + agua.ToString());
+
+
+            txtLadrilloAlto.Clear();
+            txtLadrilloAncho.Clear();
+            txtLadrilloLargo.Clear();
+            txtMurLargo.Clear();
+            txtMuroAlto.Clear();
+
+        }
+    }
+}
